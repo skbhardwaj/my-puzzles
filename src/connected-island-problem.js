e2b0f819a535bdde40fe2123c8d3be0e9ea1a3d5
Shrikrishna Bhardwaj <shrikrishna.it@gmail.com>
2019-02-08 12:41:04 +0530

    added the connected island problem

---
+++ src/connected-island-problem.js
@@ -0,0 +1,46 @@
+/*
+THE PROBLEM STATEMENT:
+======================
+The Connected Island Problem
+
+Given the data of islands in form of 0s & 1s, wherein 0s represent water & 1s represent land.
+We need to find all the connected islands(connected 1s) from the data provided.
+Hint: Consider all 8 directions! [E,W,N,S,NE,NW,SE,SW]
+
+The input would be in the form of 3 lines:
+1st line = number of test cases
+2nd line = size of the matrix
+3rd line = data of matrix
+
+EXAMPLE:
+========
+Input(visual):
+--------------
+1
+5 5
+0001010011110000100001110
+
+i.e. visually,
+
+          0, 1, 2, 3, 4
+    [
+0        [0, 0, 0, 1, 0],
+1        [1, 0, 0, 1, 1],
+2        [1, 1, 0, 0, 0],
+3        [0, 1, 0, 0, 0],
+4        [0, 1, 1, 1, 0]
+    ]
+
+Output:
+-------
+Total Islands: 2
+
+Island 1:
+[[0][3], [1][3], [1][4]]
+
+Island 2:
+[[1][0], [2][0], [2][1], [3][1], [4][1], [4][2], [4][3]]
+
+*/
+
+console.log("hello");
